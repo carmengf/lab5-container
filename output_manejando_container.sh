4d6a6a59f2fd8d60fa7d6c7f5de47fc0cba0f8c0
carmengf <carmen.gonzalez1@um.es>
2022-01-23 17:49:19 +0100

    Manejando containers

---
+++ output_manejando_container.sh
@@ -0,0 +1,46 @@
+La versión del sistema operativo de la imagen es: 
+PRETTY_NAME="Debian GNU/Linux 10 (buster)"
+NAME="Debian GNU/Linux"
+VERSION_ID="10"
+VERSION="10 (buster)"
+VERSION_CODENAME=buster
+ID=debian
+HOME_URL="https://www.debian.org/"
+SUPPORT_URL="https://www.debian.org/support"
+BUG_REPORT_URL="https://bugs.debian.org/"
+                                  
+El contenido del directorio que estamos viendo es: 
+total 80
+-rw-rw-r--. 1 alumno08 alumno08   520 Jan 23 17:46 RESULTADOS.txt
+-rwxrwxr-x. 1 alumno08 alumno08  1434 Jan 23 17:36 k-mer.cpp
+-rwxrwxr-x. 1 alumno08 alumno08   948 Jan 23 17:36 k-mer.py
+-rwxrwxr-x. 1 alumno08 alumno08 18336 Jan 23 17:36 k-mer13
+-rwxrwxr-x. 1 alumno08 alumno08  1434 Jan 23 17:36 k-mer13.cpp
+-rwxrwxr-x. 1 alumno08 alumno08   948 Jan 23 17:36 k-mer13.py
+-rwxrwxr-x. 1 alumno08 alumno08 18336 Jan 23 17:36 k-mer14
+-rwxrwxr-x. 1 alumno08 alumno08  1434 Jan 23 17:36 k-mer14.cpp
+-rwxrwxr-x. 1 alumno08 alumno08   948 Jan 23 17:36 k-mer14.py
+-rwxrwxr-x. 1 alumno08 alumno08  1580 Jan 23 17:41 manejando_container.sh
+-rw-rw-r--. 1 alumno08 alumno08   400 Jan 23 17:47 output_manejando_container.sh
+-rwxrwxr-x. 1 alumno08 alumno08   482 Jan 23 17:36 submit_eval_nativa.sh
+                                  
+El path del directorio en el que estamos es: 
+/home/alumno08/lab5-container
+                                  
+La versión de python es: 
+Python 3.9.1
+                                  
+La versión de C++ es: 
+g++ (Debian 8.3.0-6) 8.3.0
+Copyright (C) 2018 Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+                                  
+El tiempo de ejecución del k-mers de longitud 13 es: 
+FATAL:   stat /home/alumno08/lab5-container/data/k-mer13: no such file or directory
+
+real	0m0,203s
+user	0m0,077s
+sys	0m0,089s
+                                  
